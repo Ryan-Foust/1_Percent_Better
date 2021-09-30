eb55bd02b8832198f994f10ffba4c8a187214ff9
rjf2621 <rjf2621@rit.edu>
2021-09-30 09:55:15 -0400

    Control variable to control how to accept input If statements reflect the new control variable

--- src/One_Percent_Better.java
+++ src/One_Percent_Better.java
@@ -4,7 +4,26 @@ public class One_Percent_Better {
      * from @AJA_Cortes on twitter
      */
 
-    public static void main (String []args) {
-        Scanner keyboard = new Scanner(System.in);
+    public static void main (String[] args) {
+        // where will the input come from? 1 for the String []args, 0 for Scanner/user input
+        int control = Integer.parseInt(args[0]);
+
+        // invalid number
+        if (control >= 2) {
+            System.out.println("control variable was not good.");
+        }
+
+        // 0 for Scanner/user input
+        else if (control == 0) {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("control variable was 0.");
+        }
+
+        // 1 for the String []args
+        else {
+            System.out.println("control variable was 1.");
+        }
     }
+
+
 }
