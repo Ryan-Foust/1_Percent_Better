8c6487a3ff2c4da7da7a7c1129a33c9c750dcb60
rjf2621 <rjf2621@rit.edu>
2021-09-30 09:37:33 -0400

    .gitignore ignore IntelliJ files One_Percent_Better.java added main method

--- src/One_Percent_Better.java
+++ src/One_Percent_Better.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 public class One_Percent_Better {
     /**
      * from @AJA_Cortes on twitter
      */
 
     public static void main (String []args) {
-        
+        Scanner keyboard = new Scanner(System.in);
     }
 }
